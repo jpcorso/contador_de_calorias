6a53239975eff48f57e3e8e83ccc9c72cae4640d
João Pedro Licks Corso <jp.corso123@gmail.com>
2022-09-17 01:49:16 -0300

    adição de arquivos iniciais

---
+++ contador_calorias.c
@@ -0,0 +1,5 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+// insere biblioteca
+#include "contador_calorias.h"
---
+++ contador_calorias.h
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+// estrutura da árvore
+/************************************/
+typedef struct
+{
+    
+}ARVORE;
+
