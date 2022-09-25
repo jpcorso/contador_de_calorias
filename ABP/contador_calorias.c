2fe9bdd7ede81a6896c44f3ef2c60c989d447264
julia-ms <mombachdasilva@gmail.com>
2022-09-25 18:22:20 -0300

    Update contador_calorias.c

--- ABP/contador_calorias.c
+++ ABP/contador_calorias.c
@@ -4,23 +4,6 @@
 // insere biblioteca
 #include "contador_calorias.h"
 
-// insere arvore abp
-abp *insereArvore(abp *arvore, tipoInfo info)
-{
-    if (arvore == NULL)
-    {
-        arvore = (abp *)malloc(sizeof(abp));
-        arvore->info = info;
-        arvore->esquerda = NULL;
-        arvore->direita = NULL;
-        return arvore;
-    }
-    else if (info < arvore->info)
-        arvore->esquerda = InsereArvore(arvore->esquerda, info);
-    else if (info > arvore->info)
-        arvore->direita = InsereArvore(arvore->direita, info);
-    return arvore;
-}
 
 void leArquivos(char arquivo[], char alimentos, int calorias)
 {
@@ -46,5 +29,61 @@ void leArquivos(char arquivo[], char alimentos, int calorias)
     }
 
 
-
 }
+
+// insere arvore abp
+abp *insereArvore(abp *arvore, tipoInfo info)
+{
+    if (arvore == NULL)
+    {
+        arvore = (abp *)malloc(sizeof(abp));
+        arvore->info = info;
+        arvore->esquerda = NULL;
+        arvore->direita = NULL;
+        return arvore;
+    }
+    else if (info < arvore->info)
+        arvore->esquerda = InsereArvore(arvore->esquerda, info);
+    else if (info > arvore->info)
+        arvore->direita = InsereArvore(arvore->direita, info);
+    return arvore;
+}
+
+//busca alimento na arvore
+
+abp* consultaABP(abp *arvore, char chave) {
+    while (arvore != NULL){
+        if (arvore->info == chave )
+            return arvore; //achou retorna o ponteiro para o nó
+        else
+            if (arvore->info > chave)
+                arvore = arvore->esquerda;
+            else
+                arvore = arvore->direita;
+    }
+    return NULL; //não achou, retorna null
+}
+
+
+
+
+/*
+// calcula o numero de comparacoes
+pNodoA* nro_comparacoes(abp *arvore, char *chave){
+    while (a!=NULL){
+        comp++;
+            if (!strcmp(arvore->info,chave)){
+                comp++;
+                return arvore;
+            }
+            else {
+                comp++;
+                if (strcmp(arvore->info,chave)>0)
+                    arvore = arvore->esquerda;
+                else
+                    arvore = arvore->direita;
+                }
+            }
+     return NULL;
+}
+*/
