5dc1ff885b17c2f9f823b04ad528feabd008a879
julia-ms <mombachdasilva@gmail.com>
2022-09-28 01:23:58 -0300

    Update main.c

--- ABP/main.c
+++ ABP/main.c
@@ -10,10 +10,33 @@ tabela de calorias deverá ficar armazenada em uma árvore. */
 #include <stdio.h>
 #include <stdlib.h>
 
-main(void)
-{
+main(int argc, char *argv[]){
+
     abp *arvore = NULL;
     abp *aux = NULL;
+    abp dados;
+
+    if((argc < 2) || strlen(argv[1]) < 3){
+        fprintf(stderr, "Informe o nome do arquivo a ser lido, nome deve possuir ao menos 3 caracteres\n");
+        return 1;
+    }
+    FILE* arq = fopen(argv[1], "r");
+    if(arq == NULL){
+        fprintf(stderr, "Arquivo %s nao existe\n", argv[1]);
+    }
+
+    while(fscanf(arq, "%s, %d", )
+
+
+
+
+
+
+
+   
+
+
+
 
     return 0;
 }
