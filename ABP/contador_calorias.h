592d479a74121b766e7bb73dad14d069a6241a2a
julia-ms <mombachdasilva@gmail.com>
2022-09-28 01:23:55 -0300

    Update contador_calorias.h

--- ABP/contador_calorias.h
+++ ABP/contador_calorias.h
@@ -11,14 +11,25 @@ struct ARVORE
     tipoInfo info;
     struct ARVORE *esquerda;
     struct ARVORE *direita;
+    int calorias;
+    char alimento[100];
 };
 typedef struct ARVORE abp;
-
+/*
 struct ARQUIVO
 {
     int calorias;
     char alimentos[50];
 };
 typedef struct ARQUIVO dados;
+*/
 
-abp *insereArvore(abp *arvore, tipoInfo info);
+abp *insereArvore(abp *arvore, char chave);
+
+abp* consultaABP(abp *arvore, char chave);
+
+int qtd_nodos(abp *arvore);
+
+int altura(abp *arvore);
+
+abp* nro_comparacoes(abp *arvore, char *chave);
